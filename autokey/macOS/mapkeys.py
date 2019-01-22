cf42739eb565b161ca68e13f3c16e7a38370c0db
Todd Kulesza <tkulesza@google.com>
2019-01-22 15:16:25 -0800

    Support macOS shortcuts for switching tabs in GNOME Terminal.

--- autokey/macOS/mapkeys.py
+++ autokey/macOS/mapkeys.py
@@ -1,4 +1,7 @@
 import re
+import logging
+
+logging.debug("mapkeys running")
 
 active_class = window.get_active_class()
 if re.match('.*(Emacs|Gvim)', active_class):
@@ -7,6 +10,14 @@ if re.match('.*(Emacs|Gvim)', active_class):
 elif re.match('.*(Gnome-terminal)', active_class):
     original = store.get_global_value('hotkey')
     mapped_modifiers = re.sub(r'<super>', r'<ctrl>', original)
+    logging.debug("mapped_modifiers: " + str(mapped_modifiers))
+    
+    # Support global navigation shortcuts
+    if mapped_modifiers == "<ctrl>+<shift>+]":
+        mapped_modifiers = "<ctrl>+<page_down>"
+    elif mapped_modifiers == "<ctrl>+<shift>+[":
+        mapped_modifiers = "<ctrl>+<page_up>"
+    
     keyboard.send_keys(mapped_modifiers)
 else:
     mapped_keys = engine.get_return_value()
